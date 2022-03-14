72f21c7688f6483dfe2a04a70a7a6697fc9f3d55
Danilo Santana e Silva <danilo_silva@hst.com.br>
2022-03-14 14:01:14 -0300

    Aula 87

--- src/main/java/br/com/hst/pdi/libraryapi/LibraryApiApplication.java
+++ src/main/java/br/com/hst/pdi/libraryapi/LibraryApiApplication.java
@@ -1,33 +1,14 @@
 package br.com.hst.pdi.libraryapi;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import br.com.hst.pdi.libraryapi.service.EmailService;
-
 @SpringBootApplication
 @EnableScheduling
 public class LibraryApiApplication {
-
-  @Autowired
-  private EmailService emailService;
-
-  @Bean
-  public CommandLineRunner runner(){
-    return args -> {
-      List<String> emails = Arrays.asList("d6ae7f43b1-763591@inbox.mailtrap.io");
-      emailService.sendMails("Testando serviço de emails.", emails);
-      System.out.println("EMAILS ENVIADOS");
-    };
-  }
 
   @Bean
   public ModelMapper modelMapper() {
